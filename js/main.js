c1c8bc50178c003b8a85e2a61f266cccdbffabe5
YaroslavNE-Frontend <yaroslav.nov2001@gmail.com>
2023-11-22 18:44:57 +0900

    Добавляет css footer

--- js/main.js
+++ js/main.js
@@ -367,6 +367,26 @@ blockSlider.addEventListener('mouseleave', () => {
     TimerImage = setInterval(() => slowSlider(), second)
 })
 
+// Arrow Scrol-top
+
+window.onscroll = function() {
+    let scrollElem = document.getElementById("main-footer__arrow");
+    if (document.documentElement.scrollTop > document.documentElement.clientHeight) {
+        scrollElem.style.opacity = "1";
+    } else {
+        scrollElem.style.opacity = "0";
+    }
+}
+
+let timeOut;
+function goUp() {
+    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0,-100);
+        timeOut = setTimeout('goUp()',20);
+    } else clearTimeout(timeOut);
+}
+
 
 
 
